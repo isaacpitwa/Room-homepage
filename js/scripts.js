46737bf5b419bb8d5478ca889757455afddb920f
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-10-26 01:28:20 +0300

    Initiaite Slider

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("slide");
+    var slides = document.getElementsByClassName("slide-item");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
