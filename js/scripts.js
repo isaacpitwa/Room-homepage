2017d5370cd0cd0b00542ab25561e6333494a273
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-10-26 01:22:06 +0300

    Remove slider indicator

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,6 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slide");
-    var dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
