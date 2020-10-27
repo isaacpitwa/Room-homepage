64074e24a73138038939ba33dd845f527e8818ea
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-10-28 01:30:57 +0300

    Toggle Button Styles

--- js/scripts.js
+++ js/scripts.js
@@ -24,12 +24,12 @@ function showSlides(n) {
 
 function toggleNavMenu(){
     var x = document.getElementById("header");
-    if (x.className === "header") {
+    if (x.className === "header-nav") {
       x.className += " responsive";
       var humburgerBtn = document.getElementById("toggleBtn");
       humburgerBtn.className += " opened"
     } else {
-      x.className = "header";
+      x.className = "header-nav";
       console.log("Have not Updated header")
     }
 }
